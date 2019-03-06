9fb775e8d779087d05bf1a705b059b25ec7b6ce4
Hsb511 <hugo-selle@hotmail.fr>
2019-03-06 21:51:53 +0100

    the showers state are displayed by images

--- newGoogleHome/app/src/main/java/team23/speechrecognition/ShowerActivity.java
+++ newGoogleHome/app/src/main/java/team23/speechrecognition/ShowerActivity.java
@@ -1,13 +1,23 @@
 package team23.speechrecognition;
 
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.apache.xerces.impl.xpath.regex.Match;
 import org.cybergarage.upnp.Service;
 import org.cybergarage.upnp.UPnP;
 import org.cybergarage.upnp.device.InvalidDescriptionException;
@@ -18,29 +28,26 @@ import java.io.InputStream;
 public class ShowerActivity extends AppCompatActivity {
     protected TextView infoTV;
     protected TextView showerTV;
+    protected LinearLayout stateLayout;
     protected String myBuilding;
     protected String myFloor;
     private AppliDevice cyberlinkDevice;
+    protected int screenWidth;
+    protected int showerImageHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shower);
-
         infoTV = findViewById(R.id.info_textView);
-        SharedPreferences preferences = getSharedPreferences(InformationActivity.MY_PREFERENCES, MODE_PRIVATE);
-        myBuilding = preferences.getString(InformationActivity.MY_BUILDING, "i1");
-        myFloor = preferences.getString(InformationActivity.MY_FLOOR, "1");
-        String displayInfo = getResources().getString(R.string.building) + " : "
-                + myBuilding + " - "
-                + getResources().getString(R.string.floor) + " : "
-                + myFloor;
-        infoTV.setText(displayInfo);
+        stateLayout = findViewById(R.id.show_showers_state_layout);
 
-        showerTV = findViewById(R.id.available_shower);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenWidth = displayMetrics.widthPixels;
 
-        //TODO: RECUPERE L'INFOS DU NOMBRE DE DOUCHES A MON BATIMENT ET A MON ETAGE
-        showerTV.setText("3");
+
+        showShowerInfo();
 
         UPnP.setEnable(UPnP.USE_ONLY_IPV4_ADDR);
         Log.d("DEVICE","should start device");
@@ -91,5 +98,44 @@ public class ShowerActivity extends AppCompatActivity {
     public void onStop() {
         super.onStop();
         cyberlinkDevice.stop();
+    }
+
+    protected void showShowerInfo() {
+        SharedPreferences preferences = getSharedPreferences(InformationActivity.MY_PREFERENCES, MODE_PRIVATE);
+        myBuilding = preferences.getString(InformationActivity.MY_BUILDING, "i1");
+        myFloor = preferences.getString(InformationActivity.MY_FLOOR, "1");
+        String displayInfo = getResources().getString(R.string.building) + " : "
+                + myBuilding + " - "
+                + getResources().getString(R.string.floor) + " : "
+                + myFloor;
+        infoTV.setText(displayInfo);
+
+        showerTV = findViewById(R.id.available_shower);
+
+        //TODO: RECUPERE L'INFOS DU NOMBRE DE DOUCHES A MON BATIMENT ET A MON ETAGE
+        int showers = 3;
+        showerTV.setText(String.valueOf(showers));
+
+        for (int i = 0; i < showers; i++) {
+            ImageView iv = new ImageView(getApplicationContext());
+            int showerWidth = Math.round((screenWidth - 32)/showers);
+            int showerHeight = Math.round(showerWidth * 13 / 6);
+            Bitmap image = convertToBitmap(getResources().getDrawable(R.drawable.free_shower), showerWidth, showerHeight);
+            if (i == showers - 1) {
+                image = convertToBitmap(getResources().getDrawable(R.drawable.using_shower), showerWidth, showerHeight);
+            }
+            iv.setImageBitmap(image);
+            stateLayout.addView(iv);
+            Log.i("shower_debug", "showers ImageView : " + stateLayout.getChildCount());
+        }
+    }
+
+    public Bitmap convertToBitmap(Drawable drawable, int widthPixels, int heightPixels) {
+        Bitmap mutableBitmap = Bitmap.createBitmap(widthPixels, heightPixels, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(mutableBitmap);
+        drawable.setBounds(0, 0, widthPixels, heightPixels);
+        drawable.draw(canvas);
+
+        return mutableBitmap;
     }
 }
